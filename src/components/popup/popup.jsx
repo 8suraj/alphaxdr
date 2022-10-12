041ae313169a79bfa6fcc3bc0cb7f3b772f48ed8
paulsaurav <sauravpaul03@gmail.com>
2022-10-12 14:52:20 +0530

    popup

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
-import Contact from '../contact/contact';
+// import React from 'react';
+// import Contact from '../contact/contact';
 
-const popup = (vis, op) => (
-  <Contact style={{ visibility: vis,
-    opacity: op }}
-  />
-);
+// const popup = (vis, op) => (
+//   <Contact style={{ visibility: vis,
+//     opacity: op }}
+//   />
+// );
 export default popup;
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+render(<Example />);
