796f8bf72d0f17d4cafbd33e37b5ff89d9a5caf9
ShAaDow84 <basefrequency84@gmail.com>
2022-10-14 00:49:44 +0530

    cta fix

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './cta.css';
+import { Link } from 'react-router-dom';
 
 const CTA = () => (
   <div className="gpt3__cta">
@@ -8,7 +9,7 @@ const CTA = () => (
       <h3>Request a demo Today & start exploring the endless possibilities.</h3>
     </div>
     <div className="gpt3__cta-btn">
-      <button type="button">Get Started</button>
+      <button type="button"><Link to="/contactus">Get Started</Link></button>
     </div>
   </div>
 );
