22559650e604284d27db69bc29a67cb76c63d7df
ShAaDow84 <basefrequency84@gmail.com>
2022-10-14 16:20:37 +0530

    -v1.1

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,11 +3,11 @@ import possibilityImage from '../../assets/poss.png';
 import './possibility.css';
 
 const Possibility = () => (
-  <div className="alpha_possibility section__padding" id="possibility">
-    <div className="alpha_possibility-image">
+  <div className="alpha__possibility section__padding" id="possibility">
+    <div className="alpha__possibility-image">
       <img src={possibilityImage} alt="possibility" />
     </div>
-    <div className="alpha_possibility-content">
+    <div className="alpha__possibility-content">
       <h1 className="gradient__text">An Active Defence enabled <br /> Cybersecurity platform.</h1>
       <p>A scalable platform which will kill the traditional alert fatigue - SOC problem and will only notify validated cyber security breaches and incidents to its customers which is again bundled in an easy-to-deploy model and cost-effective solutions.</p>
     </div>
